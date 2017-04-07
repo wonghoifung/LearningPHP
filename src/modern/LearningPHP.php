41abbd4f7ba39a0e683a9ef00a4405f9b35f2643
huanghaifeng1 <huanghaifeng1@jd.com>
2017-04-07 15:29:48 +0800

    first commit

---
+++ src/modern/LearningPHP.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace learning\php\modern;
+
+class LearningPHP
+{
+	protected $urls;
+	protected $httpClient;
+	public function __construct(array $urls)
+	{
+		$this->urls = $urls;
+		$this->httpClient = new \GuzzleHttp\Client();
+	}
+	public function getInvalidUrls()
+	{
+		$invalidUrls = [];
+		foreach ($this->urls as $url) {
+			try {
+				$statusCode = $this->getStatusCodeForUrl($url);
+			} catch (\Exception $e) {
+				$statusCode = 500;
+			}
+			if ($statusCode >= 400) {
+				array_push($invalidUrls, [
+					'url' => $url,
+					'status' => $statusCode
+				]);
+			}
+		}
+		return $invalidUrls;
+	}
+	protected function getStatusCodeForUrl($url)
+	{
+		$httpResponse = $this->httpClient->options($url);
+		return $httpResponse->getStatusCode();
+	}
+}
